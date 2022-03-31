8e8bddfdb33375d2739070e4b81b2bbd383158aa
ismaelMamo <83426021+ismaelMamo@users.noreply.github.com>
2022-03-30 18:27:34 -0600

    update display image
    Putting the display image in the correct format

--- src/component/display.tsx
+++ src/component/display.tsx
@@ -1,12 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import data from "../data/sneakers_data.json";
 import "../style/display.css";
 
+type Sneakers = {
+	id: string;
+	name: string;
+	img: { display: string; preview: string }[];
+}[];
+type PrevImages = { display: string; preview: string }[];
+
 function Display() {
-	const [Images, setImages] = useState("");
+	const [Images, setImages] = useState("../images/image-product-1.jpg");
+	const [PrvImages, setPrvImages]: any[] = useState([]);
+
+	let Data: Sneakers = [];
+	useEffect(() => {
+		Data = data;
+		//Should retrieve already formate data
+		// let temp: PrevImages = Data[0].img;
+		// temp.forEach((element) => {
+		// 	setPrvImages(...PrvImages, require(element.preview.toString()));
+		// 	console.log(require(element.preview));
+		// });
+		// console.log(PrvImages);
+	}, [Images, setPrvImages]);
+
 	return (
 		<div className='display_container'>
 			<div className='main_img_container'>
-				<img className='main_img' />
+				<img src={`Images`.toString()} className='main_img' />
+			</div>
+			<div>
+				{/* {Data[0].img.map((data) => {
+					console.log(data.preview);
+					return <img src={data.preview} />;
+				})} */}
 			</div>
 			<div className='preview_img'></div>
 		</div>
